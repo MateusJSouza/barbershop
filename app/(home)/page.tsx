019be89f5f0a06be122a0d24679df66551e82517
MateusJSouza <mateusjdev@gmail.com>
2024-01-30 20:22:30 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,22 @@
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import { Header } from '../_components/header'
 
 export default function Home() {
-  return <Header />
+  return (
+    <div>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+
+        <p className="capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+    </div>
+  )
 }
