02552a50a53493c79de7c8c559a9f29fa56b0b3c
MateusJSouza <mateusjdev@gmail.com>
2024-02-05 10:54:29 -0300

    feat: add footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { Footer } from './_components/footer'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>{children}</body>
+      <body className={`${inter.className} dark`}>
+        {children}
+
+        <Footer />
+      </body>
     </html>
   )
 }
