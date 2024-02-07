4dd5fa450fc28776b7b326f7069fef3e52379fd0
MateusJSouza <mateusjdev@gmail.com>
2024-02-07 12:01:54 -0300

    feat: ensure user can search for barbershops

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -75,8 +75,8 @@ export default async function Home() {
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
-            <div key={barbershop.id}>
-              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem barbershop={barbershop} />
             </div>
           ))}
         </div>
